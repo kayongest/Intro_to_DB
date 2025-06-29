3109192e586f7f0d8e4fa9d3b3a9e701e746784f
kayongest <kayonga70@gmail.com>
2025-06-29 22:31:57 +0200

    Out comment

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,151 +1,73 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 29, 2025 at 10:28 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE IF NOT EXISTS `alx_book_store`;
+
+USE `alx_book_store`;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE
+    `authors` (
+        `author_id` INT (11) NOT NULL,
+        `author_name` VARCHAR(215) NOT NULL
+    ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
---
--- Database: `alx_book_store`
---
 
--- --------------------------------------------------------
+CREATE TABLE
+    `books` (
+        `book_id` INT (11) NOT NULL,
+        `title` VARCHAR(130) NOT NULL,
+        `author_id` INT (11) DEFAULT NULL,
+        `price` DOUBLE NOT NULL,
+        `publication_date` DATE DEFAULT NULL
+    ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
---
--- Table structure for table `authors`
---
 
-CREATE TABLE `authors` (
-  `author_id` int(11) NOT NULL,
-  `author_name` varchar(215) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE
+    `customers` (
+        `customer_id` INT (11) NOT NULL,
+        `customer_name` VARCHAR(215) NOT NULL,
+        `email` VARCHAR(215) DEFAULT NULL,
+        `address` TEXT DEFAULT NULL
+    ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `books`
---
 
-CREATE TABLE `books` (
-  `book_id` int(11) NOT NULL,
-  `title` varchar(130) NOT NULL,
-  `author_id` int(11) DEFAULT NULL,
-  `price` double NOT NULL,
-  `publication_date` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE
+    `orders` (
+        `order_id` INT (11) NOT NULL,
+        `customer_id` INT (11) DEFAULT NULL,
+        `order_date` DATE NOT NULL
+    ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `customers`
---
+CREATE TABLE
+    `order_details` (
+        `orderdetailid` INT (11) NOT NULL,
+        `order_id` INT (11) DEFAULT NULL,
+        `book_id` INT (11) DEFAULT NULL,
+        `quantity` DOUBLE NOT NULL
+    ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
-CREATE TABLE `customers` (
-  `customer_id` int(11) NOT NULL,
-  `customer_name` varchar(215) NOT NULL,
-  `email` varchar(215) DEFAULT NULL,
-  `address` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE `authors` ADD PRIMARY KEY (`author_id`);
 
--- --------------------------------------------------------
+ALTER TABLE `books` ADD PRIMARY KEY (`book_id`),
+ADD KEY `author_id` (`author_id`);
 
---
--- Table structure for table `orders`
---
+ALTER TABLE `customers` ADD PRIMARY KEY (`customer_id`);
 
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  `order_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE `orders` ADD PRIMARY KEY (`order_id`),
+ADD KEY `customer_id` (`customer_id`);
 
--- --------------------------------------------------------
+ALTER TABLE `order_details` ADD PRIMARY KEY (`orderdetailid`),
+ADD KEY `order_id` (`order_id`),
+ADD KEY `book_id` (`book_id`);
 
---
--- Table structure for table `order_details`
---
 
-CREATE TABLE `order_details` (
-  `orderdetailid` int(11) NOT NULL,
-  `order_id` int(11) DEFAULT NULL,
-  `book_id` int(11) DEFAULT NULL,
-  `quantity` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE `books` ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`);
 
---
--- Indexes for dumped tables
---
+ALTER TABLE `orders` ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
 
---
--- Indexes for table `authors`
---
-ALTER TABLE `authors`
-  ADD PRIMARY KEY (`author_id`);
+ALTER TABLE `order_details` ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`);
 
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`book_id`),
-  ADD KEY `author_id` (`author_id`);
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`customer_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Indexes for table `order_details`
---
-ALTER TABLE `order_details`
-  ADD PRIMARY KEY (`orderdetailid`),
-  ADD KEY `order_id` (`order_id`),
-  ADD KEY `book_id` (`book_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `books`
---
-ALTER TABLE `books`
-  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
-
---
--- Constraints for table `order_details`
---
-ALTER TABLE `order_details`
-  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
-  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
