31d6785c485856917b1467bef98b128886a66567
kayongest <kayonga70@gmail.com>
2025-07-04 12:57:26 +0200

    Update task_5.sql - 2

--- task_5.sql
+++ task_5.sql
@@ -1,11 +1,5 @@
 -- Script to insert a single customer record
 -- File: task_5.sql
-INSERT INTO
-    customer (customer_id, customer_name, email, address)
-VALUES
-    (
-        1,
-        'Cole Baidoo',
-        'cbaidoo@sandtech.com',
-        '123 Happiness Ave.'
-    );
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
