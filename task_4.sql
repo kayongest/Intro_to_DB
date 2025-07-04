842f61bf6950a7cbcd009e57a5036c244a371e12
kayongest <kayonga70@gmail.com>
2025-07-04 12:51:12 +0200

    Add task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Script to show books table structure
+-- File: task_4.sql
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
